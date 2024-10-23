f95d3ed91d47dc885f985cf76e56969f9eb1cd2a
Carlos Eduardo - PC_Casa <eduardo201184@hotmail.com>
2024-10-22 22:57:06 -0300

    feat: installing tailwind

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,9 @@
-import './App.css'
+import './App.css';
 
 export default function App() {
-
-
   return (
     <div>
-      <h1>Hello word</h1>
+      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
     </div>
-  )
+  );
 }
